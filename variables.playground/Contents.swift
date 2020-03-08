e1592e6ac6b6cc7d63a1a5c0fb2424e65169de76
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2020-03-07 23:32:18 -0300

    Adds tuples structure.

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -3,3 +3,17 @@ var address = "201 Main Street"
 print("\(name) lives at \(address)")
 address = "301 Fifth Avenue"
 print("\(name) lives at \(address)")
+
+// Tuple
+var dialCode = 44
+var isoCode = "GB"
+var nameCountry = "United Kingdom"
+//var country = (44, "GB", "United kingdom")
+//print(country.0)
+//print(country.1)
+//print(country.2)
+
+var country = (dialCode: 44, isoCode: "GB", name: "United kingdom")
+print(country.dialCode)
+print(country.isoCode)
+print(country.name)
