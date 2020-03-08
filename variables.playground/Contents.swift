68211f0645b3251de9c3f65d37ce7ac35998a3e7
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2020-03-08 00:34:51 -0300

    Add optional variables.

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -24,3 +24,13 @@ print("Country Name: ", ret.name)
 
 let person1 = ("John Dow", "201 Main Street", 35)
 print("\(person1.0) lives at \(person1.1) and is \(person1.2) years old.")
+
+let person2 = (name: "John Dow", address: "301 Fifth Avenue", age: 35)
+print("\(person2.name) lives at \(person2.address) and is \(person2.age) years old.")
+
+var nameNonOptional: String = "Hello"
+var nameOptional: String?
+
+print(nameNonOptional)
+print(nameOptional)
+
