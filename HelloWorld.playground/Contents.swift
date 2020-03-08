c717132c9d69ca30c0a5dfbc41a4707a3f83d592
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2020-03-07 22:20:17 -0300

    [Mavro] Add variable and constants, any type.

--- HelloWorld.playground/Contents.swift
+++ HelloWorld.playground/Contents.swift
@@ -3,3 +3,19 @@ print(message)
 
 var pi : Double = 3.14159
 print(pi)
+print(Float.pi)
+print(Double.pi)
+
+var pi1 = 3.14159
+let pi2 = 3.14159
+
+pi1 = pi1 * 2
+
+print(pi1)
+
+var name = "George Smith"
+
+var anyType: Any
+
+anyType = "Hello, Wordl"
+anyType = 3.14159
